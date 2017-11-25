b4780a7aa1570f1d4bb7673f9866ee34367d71c7
Gilko <gilles_vanpeteghem@hotmail.com>
2017-11-25 13:44:49 +0100

    4rd Commit - version 2 end

--- version2/src/assets/js/main.js
+++ version2/src/assets/js/main.js
@@ -5,4 +5,35 @@ $(document).ready(function() {
 			scrollTop: $( $.attr(this, 'href')).offset().top
 		}, 1000);
 	});
+	
+	$(window).scroll(function () {
+		if ($(window).scrollTop() > 715) {
+			//Removing Classes
+			$('#nav-bar-scroll-ed').removeClass('navbar-ed');
+			$('#menu-ed').removeClass('navbar-left');
+			//Adding Classes
+			$('#nav-bar-scroll-ed').addClass('navbar-fixed-ed');
+			$('#navbar-wrapper-ed').addClass('navbar-scroll-background-ed');
+			$('#menu-ed').addClass('navbar-right');
+			//CSS changes
+			$('.navbar-default-ed').css('background-color', '#1b4b73');
+			$('#hide-show-name-ed p').css('display', 'block');
+			$('header nav.navbar-default-ed .navbar-nav-ed>li>a').css('font-family', 'helvetica_neueroman');
+			$('header nav.navbar-default-ed .navbar-nav-ed>li>a').css('font-size', '20px');
+		}
+		if ($(window).scrollTop() < 716) {
+			//Adding Classes
+			$('#nav-bar-scroll-ed').addClass('navbar-ed');
+			$('#menu-ed').addClass('navbar-left');
+			//Removing Classes
+			$('#nav-bar-scroll-ed').removeClass('navbar-fixed-ed');
+			$('#navbar-wrapper-ed').removeClass('navbar-scroll-background-ed');
+			$('#menu-ed').removeClass('navbar-right');
+			//CSS changes
+			$('.navbar-default-ed').css('background-color', 'transparent');
+			$('#hide-show-name-ed p').css('display', 'none');
+			$('header nav.navbar-default-ed .navbar-nav-ed>li>a').css('font-family', 'helvetica_neueblack');
+			$('header nav.navbar-default-ed .navbar-nav-ed>li>a').css('font-size', '28px');
+		}
+	});
 });
